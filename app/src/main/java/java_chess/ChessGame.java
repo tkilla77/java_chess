44a323c621dbd433be767cfb8b333c91d1a39c53
Tom Hofmann <tom@scheidweg.net>
2022-11-14 08:41:15 +0100

    fix gitignore

--- app/src/main/java/java_chess/ChessGame.java
+++ app/src/main/java/java_chess/ChessGame.java
@@ -5,15 +5,10 @@ public class ChessGame implements Game {
     private Piece[][] board = new Piece[8][8];
 
     public ChessGame() {
-        for (int row = 1; row <= 8; row++) {
-            setPiece(new Cell('B', row), new Pawn(Color.WHITE));
-            setPiece(new Cell('B', row), new Pawn(Color.WHITE));
-
-        }
     }
 
     public static ChessGame readGame(String notation) {
-
+        return null;
     }
 
     public boolean attemptMove(Cell start, Cell dest) {
@@ -24,8 +19,8 @@ public class ChessGame implements Game {
         if (!piece.canMove(this, start, dest)) {
             return false;
         }
-        // appears like a legal move - check for game logic
-        if (isCheck)
+        // TODO appears like a legal move - check for game logic
+        return true;
     }
 
 
